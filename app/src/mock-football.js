c0ff0c7e08c090457cdacd848ec766207a2647cf
James Drawneek <jmdrawneek@googlemail.com>
2021-03-30 17:13:38 +0100

    Added items for code'n'chat.

---
+++ app/src/mock-football.js
@@ -0,0 +1,78 @@
+export default [
+    {
+      league: 'Premier League',
+      games: [
+        {
+          game: 'Burnley v Bournemouth',
+          odds: {
+            win: '5/4',
+            draw: '9/4',
+            lose: '21/10'
+          }
+        },
+        {
+          game: 'Swansea v Manchester City',
+          odds: {
+            win: '6/4',
+            draw: '11/7',
+            lose: '4/10'
+          }
+        },
+        {
+          game: 'Sheffield v Bristol City',
+          odds: {
+            win: '3/5',
+            draw: '14/10',
+            lose: '3/2'
+          }
+        },
+        {
+          game: 'Sheffield v Bristol City',
+          odds: {
+            win: '3/5',
+            draw: '14/10',
+            lose: '3/2'
+          }
+        }
+      ]
+    },
+    {
+      league: 'Champions League',
+      games: [
+        {
+          game: 'Burnley v Bournemouth',
+          odds: {
+            win: '5/4',
+            draw: '9/4',
+            lose: '21/10'
+          }
+        },
+        {
+          game: 'Swansea v Manchester City',
+          odds: {
+            win: '6/4',
+            draw: '11/7',
+            lose: '4/10'
+          }
+        },
+        {
+          game: 'Sheffield v Bristol City',
+          odds: {
+            win: '3/5',
+            draw: '14/10',
+            lose: '3/2'
+          }
+        },
+        {
+          game: 'Sheffield v Bristol City',
+          odds: {
+            win: '3/5',
+            draw: '14/10',
+            lose: '3/2'
+          }
+        }
+      ]
+    }
+    
+  
+];
---
+++ app/src/mock-horse.js
@@ -0,0 +1,83 @@
+export default [
+    {
+      raceName: 'Grand National',
+      races: [
+        {
+          time: 1622387274,
+          competing: [
+            {
+              name: 'Melba Alles',
+              jockey: 'Macy Hadaway',
+              odds: '15/8'
+            },
+            {
+              name: 'Rocco Schuyler',
+              jockey: 'Bree Strange',
+              odds: '10/8'
+            },
+            {
+              name: 'Artemius Mac Dhubhghaill',
+              jockey: 'Kiaran Newman',
+              odds: '9/2'
+            },
+            {
+              name: 'Mert Dirkse',
+              jockey: 'Cherice Weekes',
+              odds: '7/1'
+            }
+          ]
+        },
+        {
+          time: 1622372874,
+          competing: [
+            {
+              name: 'Melba Alles',
+              jockey: 'Macy Hadaway',
+              odds: '15/8'
+            },
+            {
+              name: 'Rocco Schuyler',
+              jockey: 'Bree Strange',
+              odds: '10/8'
+            },
+            {
+              name: 'Artemius Mac Dhubhghaill',
+              jockey: 'Kiaran Newman',
+              odds: '9/2'
+            },
+            {
+              name: 'Mert Dirkse',
+              jockey: 'Cherice Weekes',
+              odds: '7/1'
+            }
+          ]
+        },
+        {
+          time: 1622365674,
+          competing: [
+            {
+              name: 'Melba Alles',
+              jockey: 'Macy Hadaway',
+              odds: '15/8'
+            },
+            {
+              name: 'Rocco Schuyler',
+              jockey: 'Bree Strange',
+              odds: '10/8'
+            },
+            {
+              name: 'Artemius Mac Dhubhghaill',
+              jockey: 'Kiaran Newman',
+              odds: '9/2'
+            },
+            {
+              name: 'Mert Dirkse',
+              jockey: 'Cherice Weekes',
+              odds: '7/1'
+            }
+          ]
+        }
+      ]
+      
+    }
+  ];
